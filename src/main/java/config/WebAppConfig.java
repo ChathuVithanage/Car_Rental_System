424e1f0c601f1d705e091cef0874b28d368b1174
chathuri <chathuriimasha60@gmail.com>
2023-02-18 11:12:17 +0530

    update packages

---
+++ src/main/java/config/WebAppConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import Controller.CustomerController;
+import asvisor.AppWideExceptionHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {CustomerController.class, AppWideExceptionHandler.class})
+public class WebAppConfig {
+
+
+}
